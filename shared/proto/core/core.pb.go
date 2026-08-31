902c5179ec16781fc3abffaabefbd624ba2c0042
Dane Albaugh <dane@augno.com>
2026-08-31 14:40:36 -0400

    chore(stainless): generate new endpoints

--- shared/proto/core/core.pb.go
+++ shared/proto/core/core.pb.go
@@ -13,11 +13,12 @@
 package core
 
 import (
+	reflect "reflect"
+	unsafe "unsafe"
+
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
-	reflect "reflect"
-	unsafe "unsafe"
 )
 
 const (
